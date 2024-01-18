94383fa2f97a4208920bfd1505189923294e318f
Timishot <timidotcom58@gmail.com>
2024-01-18 23:10:18 +0100

    git add

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -1,24 +1,41 @@
+
+
 $(document).ready(function(){
 	// Initialize an empty object to store selected empty
 	let selectedAmenities = {};
+	let selectedCities = {};
+	let selectedStateIds = [];
+	
 
 	//Listen for changes on each inpur checkbox
-	$('input[type="checkbox"]').change(function(){
+	$('input[type="checkbox"].city').change(function(){
+		updateFilter(this, selectedAmenities, 'locations')
+		let stateID = $(this).closest('li')
+		console.log(stateID)
+
+		if ($(this).prop('checked')) {
+			selectedStateIds.push(stateID);
+		} else {
+			selectedStateIds = selectedStateIds.filter(id => id !== stateID);
+		}
 		// Get the amenity ID and name from the data atributes
-		let amenityID = $(this).data('id');
-		let amenityName = $(this).data('name');
+		// let amenityID = $(this).data('id');
+		// let amenityName = $(this).data('name');
 
 		// check if the checkbox is checked
-		if ($(this).prop('checked')){
-			selectedAmenities[amenityID] = amenityName;
-		} else {
-			delete selectedAmenities[amenityID];
-		}
+		// if ($(this).prop('checked')){
+		// 	selectedAmenities[amenityID] = amenityName;
+		// } else {
+		// 	delete selectedAmenities[amenityID];
+		// }
 		
 		// Update the h4 tag inside the div Amenities with the list of Amenities checked
-		let selectedAmenitiesArray = Object.values(selectedAmenities);
-		$('.amenities h4').text(selectedAmenitiesArray.length > 2 ? selectedAmenitiesArray.slice(0, 2).join(', ') + '...' :  selectedAmenitiesArray.join(', '))
-		console.log(selectedAmenitiesArray)
+		// let selectedAmenitiesArray = Object.values(selectedAmenities);
+		// $('.amenities h4').text(selectedAmenitiesArray.length > 2 ? selectedAmenitiesArray.slice(0, 2).join(', ') + '...' :  selectedAmenitiesArray.join(', '))
+	});
+
+	$('input[type="checkbox"].amenity').change(function(){
+		updateFilter(this, selectedCities, 'amenities');
 	});
 	
 	$('.filter_button').click(function() {
@@ -27,10 +44,12 @@ $(document).ready(function(){
 			type: 'POST',
 			contentType: 'application/json',
 			data: JSON.stringify({
-				amenities: Object.keys(selectedAmenities)
+				amenities: Object.keys(selectedAmenities),
+				cities: Object.keys(selectedCities),
+				states: selectedStateIds
 			}),
 			success: function(data) {
-				console.log(data)
+				
 				const placeSection = $('.places');
 				$.each(data, function(index, place){
 					const article = $('<article>');
@@ -59,4 +78,22 @@ $(document).ready(function(){
 			}
 			})
 	})
-})
+
+	const updateFilter = (checkbox, selectedFilter, FilterType)=>{
+		let FilterID = $(checkbox).data('id');
+		let filterName = $(checkbox).data('name')
+
+		// check if the checkbox is checked
+		if ($(checkbox).prop('checked')) {
+			selectedFilter[FilterID] = filterName;
+		} else {
+			delete selectedFilter[FilterID];
+		}
+
+		// Update the h4 tag inside the div Locations with the list of selected filters
+		let selectedFilterArray = Object.values(selectedFilter);
+
+		
+		$(`.${FilterType} h4`).text(selectedFilterArray.length > 2 ? selectedFilterArray.slice(0, 2).join(', ') + '...' :  selectedFilterArray.join(', '));
+	}
+});
